125e9a0579bb6446165e2c2d8afbdcbf7f3c9cd1
aaftabv22 <aaftabvhora62@gmail.com>
2026-05-30 19:06:38 -0400

    Add console error and missing alt tag detection

--- backend/app/services/playwright_service.py
+++ backend/app/services/playwright_service.py
@@ -4,7 +4,16 @@ def capture_screenshot(url: str):
     with sync_playwright() as p:
         browser = p.chromium.launch()
         page = browser.new_page()
-        
+        console_errors = []        
+
+        #attaching to console before loading the page
+        page.on(
+            "console",
+            lambda msg: console_errors.append(msg.text)
+            if msg.type == "error"
+            else None
+        )
+
         page.goto(url)
 
         #checking if image load or not and if width is 0 after loading
@@ -17,7 +26,15 @@ def capture_screenshot(url: str):
                 .map(img => img.src);
         }
         """)
-        print("Broken Images:", broken_images)
+
+        #checking for missin alt tags
+        missing_alt_tags = page.evaluate("""
+        () => {
+            return Array.from(document.images)
+                .filter(img => !img.hasAttribute('alt') || img.alt.trim() === '')
+                .map(img => img.src);
+        }
+        """)
 
         #mobile viewPort
         page.set_viewport_size({"width":390, "height": 844})
@@ -69,6 +86,10 @@ def capture_screenshot(url: str):
                 "tablet": tablet_overflow,
                 "desktop": desktop_overflow
             },
-             "brokenImages": broken_images,
+            "brokenImages": broken_images,
             "brokenImageCount": len(broken_images),
+            "consoleErrors": console_errors,
+            "consoleErrorCount": len(console_errors),
+            "missingAltTags": missing_alt_tags,
+            "missingAltTagCount": len(missing_alt_tags)
         }
